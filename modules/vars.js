69f96177d38b2762c077a9f7d1fb4cdd3bff1432
svet-nikol <plaksina.sn@mail.ru>
2023-08-27 00:16:42 +0300

    все модули готовы

---
+++ modules/vars.js
@@ -0,0 +1,5 @@
+const ulElement = document.querySelector('ul[class="comments"]');
+const ulProgressElement = document.querySelector('div[class="comments-progress"]');
+const addFormProgressElement = document.querySelector('div[class="add-form-progress"]');
+const containerFormsElement = document.querySelector('div[class="containerForms"]');
+export { ulElement, ulProgressElement, addFormProgressElement, containerFormsElement };
