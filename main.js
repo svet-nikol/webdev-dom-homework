98aa0dfca17e8f32a56a9124f018efe9c1ab18e7
svet-nikol <plaksina.sn@mail.ru>
2023-08-30 21:16:41 +0300

    редактирую index и render

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { getComments } from "./modules/api.js";
-import { ulElement, ulProgressElement, addFormProgressElement } from "./modules/vars.js";
+// import { ulElement, ulProgressElement } from "./modules/vars.js";
 import { renderComments, renderForms } from "./modules/render.js";
 import { checkInput, addComment } from "./modules/actions.js";
 export { fetchAndRenderComments };
@@ -10,18 +10,24 @@ let comments = [];
 
 function fetchAndRenderComments() {   // ЗАГРУЗКА С СЕРВЕРА И РЕНДЕР ВСЕХ КОММЕНТАРИЕВ
   
-  ulElement.style.display = 'none';
-  ulProgressElement.style.display = 'block';
+  // ulElement.style.display = 'none';
+  // ulProgressElement.style.display = 'block';
   getComments().then((responseData) => {
       comments = responseData.comments;
       renderComments({ comments });
+      // const ulElement = document.querySelector('ul[class="comments"]');
+      // const ulProgressElement = document.querySelector('div[class="comments-progress"]');
       return true;
     })
     .then(() => {
+      const ulElement = document.querySelector('ul[class="comments"]');
+      const ulProgressElement = document.querySelector('div[class="comments-progress"]');
       ulElement.style.display = 'flex';
       ulProgressElement.style.display = 'none';
     })
     .catch((error) => {
+      // const ulElement = document.querySelector('ul[class="comments"]');
+      // const ulProgressElement = document.querySelector('div[class="comments-progress"]');
       ulElement.style.display = 'flex';
       ulProgressElement.style.display = 'none';
       alert("Кажется, у вас сломался интернет, попробуйте позже обновить страницу...");
@@ -41,6 +47,7 @@ function globalAdd() {   // ДОБАВЛЕНИЕ НОВОГО КОММЕНТАР
     const nameElement = document.querySelector('input[class="add-form-name"]');
     const textElement = document.querySelector('textarea[class="add-form-text"]');
     const addFormElement = document.querySelector('div[class="add-form"]');
+    const addFormProgressElement = document.querySelector('div[class="add-form-progress"]');
 
     checkInput({ buttonElement, nameElement, textElement });
 
--- modules/render.js
+++ modules/render.js
@@ -1,36 +1,47 @@
-import { ulElement, containerFormsElement  } from "./vars.js";
+import { containerFormsElement  } from "./vars.js";
 import { initLikeComments, initReplyComment } from "./actions.js";
 
 export { renderComments, renderForms };
 
 
-    function renderComments({ comments }) {      
-        const commentsHTML = comments.map((comment, ind) => {
-    
-        let currentTime = new Date(comment.date);
-        let commentTime = `${currentTime.toLocaleDateString('ru-Ru', { day: "2-digit", month: "2-digit", year: "2-digit" })} ${currentTime.toLocaleTimeString('ru-Ru', { hour: "2-digit", minute: "2-digit" })}`;
-    
-        return `
-            <li data-index="${ind}" class="comment">
-                <div class="comment-header">
-                    <div>${comment.author.name}</div>
-                    <div>${commentTime}</div>
-                </div>
-                <div class="comment-body">
-                    <div class="comment-text">
-                    ${comment.text.replaceAll("QUOTE_BEGIN", "<div class='quote'>").replaceAll("QUOTE_END", "</div>")}
-                    </div>
-                </div>
-                <div class="comment-footer">
-                    <div class="likes">
-                    <span class="likes-counter">${comment.likes}</span>
-                    <button data-index="${ind}" class="like-button ${comment.isLiked ? "-active-like" : ""}"></button>
-                    </div>
-                </div>
-            </li> `;
+    function renderComments({ comments }) {  
+        const appElement = document.querySelector('div[class="containerComments"]');     
+        const commentsHTML = comments
+          .map((comment, ind) => {
+          let currentTime = new Date(comment.date);
+          let commentTime = `${currentTime.toLocaleDateString('ru-Ru', { day: "2-digit", month: "2-digit", year: "2-digit" })} ${currentTime.toLocaleTimeString('ru-Ru', { hour: "2-digit", minute: "2-digit" })}`;
+          return `
+              <li data-index="${ind}" class="comment">
+                  <div class="comment-header">
+                      <div>${comment.author.name}</div>
+                      <div>${commentTime}</div>
+                  </div>
+                  <div class="comment-body">
+                      <div class="comment-text">
+                      ${comment.text.replaceAll("QUOTE_BEGIN", "<div class='quote'>").replaceAll("QUOTE_END", "</div>")}
+                      </div>
+                  </div>
+                  <div class="comment-footer">
+                      <div class="likes">
+                      <span class="likes-counter">${comment.likes}</span>
+                      <button data-index="${ind}" class="like-button ${comment.isLiked ? "-active-like" : ""}"></button>
+                      </div>
+                  </div>
+              </li> `;
         }).join("");
+
+        const appHTML = `      <ul class="comments">${commentsHTML}</ul>
+        <div class="comments-progress">
+          <p>Подождите, комментарии загружаются...</p>   <!-- для лоудера на момент загрузки всех комментариев с свервера -->
+        </div>`
     
-        ulElement.innerHTML = commentsHTML;
+        appElement.innerHTML = appHTML;
+
+        const ulElement = document.querySelector('ul[class="comments"]');
+        const ulProgressElement = document.querySelector('div[class="comments-progress"]');
+
+        ulElement.style.display = 'none';
+        ulProgressElement.style.display = 'block';
     
         initLikeComments({ comments }); 
         initReplyComment({ comments });
@@ -47,9 +58,12 @@ export { renderComments, renderForms };
         <div class="add-form-row">
           <button class="add-form-button">Написать</button>
         </div>
+      </div>
+      <div class="add-form-progress">
+      <p>Ваш комментарий добавляется...</p>
       </div>`;
 
-        
+  
     
         // функционал удаления последнего комментария
         let buttonDelete = document.querySelector('button[class="delete-form-button"]');
