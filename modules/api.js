7947edd311f0af4235a60ae7075357f3b9ce2915
svet-nikol <plaksina.sn@mail.ru>
2023-08-31 20:20:49 +0300

    draft enter and regist forms, new host API

--- modules/api.js
+++ modules/api.js
@@ -1,4 +1,5 @@
-const baseUrl = 'https://wedev-api.sky.pro/api/v2/:sveta-plaksina'
+const baseUrl = 'https://wedev-api.sky.pro/api/v2/:sveta-plaksina';
+const userUrl = 'https://wedev-api.sky.pro/api/user';
 
 
 export function getComments() {
@@ -40,4 +41,19 @@ export function postApi({ name, text }) {
       }
       return response.json();
     })
+}
+
+export function login({login, password}) {
+  return fetch(`${userUrl}/login`, {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+    }),
+  }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Плохой запрос");
+    };
+    return response.json();
+  });
 }
